6c26f4cd9c2a79b44cc58ad4b7399ac875cd7eb3
radon <radon@radon.com>
2023-05-31 22:58:51 +0100

    Theatrical elements

---
+++ src/libAssets/Script.hpp
@@ -0,0 +1,7 @@
+/*
+Equivalent of a theathre script, or a screenPlay, but for a game production
+Should represent the written script for the game
+We there should be a written script in natural language.
+
+Could be in form of graph
+*/
---
+++ src/libAssets/StageManager.hpp
@@ -0,0 +1,7 @@
+/*
+make sure everything is in place at the right time
+as specified in the script
+ensure the smooth execution according to the directors artistic vison
+*/
+
+/* Should execute the script*/
